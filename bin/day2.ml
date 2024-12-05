0dba096de3332872c17d603843353377e6c14302
chitraksen <chitraksen@gmail.com>
2024-12-05 05:30:01 +0000

    Day 2

--- bin/day2.ml
+++ bin/day2.ml
@@ -15,24 +15,67 @@ let input_file_to_2d_list file_path =
       l
   in process_lines []
 
-(** takes a report and measures if it's safe *)
-let rec is_safe l =
-  let increasing a b c = (c > b) && (b > a) && (c - b <= 3) && (b - a <= 3) in
-  let decreasing a b c = (c < b) && (b < a) && (b - c <= 3) && (a - b <= 3) in
-  match l with
-  | a :: b :: c :: rest -> (increasing a b c || decreasing a b c) && is_safe (b :: c :: rest)
-  | a :: b :: _ -> 1 <= abs (a - b) && abs (a - b) <= 3
-  | _ -> true
+(* check if difference between two elements is between 1 and 3 *)
+let is_valid_diff a b = 
+  let diff = abs (a - b) in
+  diff >= 1 && diff <= 3
+
+(* check if a list is monotonically increasing or decreasing *)
+let is_valid_report lst =
+  let rec check_monotonic trend = function
+    | [] | [_] -> true
+    | a :: b :: rest ->
+        match trend with
+        | None -> 
+            (* initial trend *)
+            if is_valid_diff a b then
+              let new_trend = if a < b then Some true else Some false
+              in check_monotonic new_trend (b :: rest)
+            else false
+        | Some is_increasing ->
+            if not (is_valid_diff a b) then false
+            else
+              (* check trend is maintained *)
+              let current_comparison = 
+                if is_increasing then a < b else a > b 
+              in
+              if not current_comparison then false
+              else check_monotonic trend (b :: rest)
+  in
+  check_monotonic None lst
+
+(** given a report and dampener availability, check if report is safe *)
+let check_report_safety lst dampening =
+  (* check if the report is already valid *)
+  if is_valid_report lst then true
+  else if dampening then
+    (* if dampener available remove each element and check if the resulting report is valid *)
+    let check_removal lst =
+      let rec check_report_without index =
+        if index >= List.length lst then false
+        else 
+          let removed = List.filteri (fun i _ -> i <> index) lst in
+          if is_valid_report removed then true
+          else check_report_without (index + 1)
+      in
+      check_report_without 0
+    in
+    check_removal lst
+  else false
 
 (** takes a list of reports as input and returns how many of them are safe *)
-let rec number_of_safe_reports data =
+let rec number_of_safe_reports data dampening =
   match data with
   | [] -> 0
-  | report :: rest -> (if is_safe report then 1 else 0) + number_of_safe_reports rest
+  | report :: rest -> (if check_report_safety report dampening then 1 else 0) + number_of_safe_reports rest dampening
 
 let () =
   let data = input_file_to_2d_list file_path in
 
   (* part 1 *)
-  let safe_reports = number_of_safe_reports data in
-  print_string "solution to part 1: "; print_int safe_reports; print_endline ""
+  let safe_reports = number_of_safe_reports data false in
+  print_string "solution to part 1: "; print_int safe_reports; print_endline "";
+
+  (* part 2 *)
+  let safe_reports_dampened = number_of_safe_reports data true in
+  print_string "solution to part 2: "; print_int safe_reports_dampened; print_endline ""
