aadad9f791210d01d2a1f1dd0b8eb4f92edf7df6
chitraksen <chitraksen@gmail.com>
2024-12-08 18:58:03 +0000

    Day 4

--- bin/day4.ml
+++ bin/day4.ml
@@ -20,8 +20,6 @@ let traversal =
     [| 0; -1 |];
   |]
 
-let search_word = "XMAS"
-
 (** take input file and convert to 2d matrix with each element being a char *)
 let input_file_to_matrix file_path =
   (* read input into string *)
@@ -35,13 +33,15 @@ let input_file_to_matrix file_path =
   Array.map (fun row -> String.to_seq row |> Array.of_seq) matrix_1d
 
 (** check if search word exists from specific position, in traversal direction *)
-let word_exists_in_dir matrix pos dir =
+let xmas_check matrix pos dir =
+  (* x already checked in main loop *)
+  let search_word = "MAS" in
   let n_rows = Array.length matrix in
   let n_cols = Array.length matrix.(0) in
   String.iteri
     (fun i letter ->
-      let i_pos = pos.(0) + (dir.(0) * i) in
-      let j_pos = pos.(1) + (dir.(1) * i) in
+      let i_pos = pos.(0) + (dir.(0) * (i + 1)) in
+      let j_pos = pos.(1) + (dir.(1) * (i + 1)) in
       (* if not valid pos, fail *)
       if i_pos < 0 || i_pos >= n_rows || j_pos < 0 || j_pos >= n_cols then
         failwith "out of bounds"
@@ -52,9 +52,35 @@ let word_exists_in_dir matrix pos dir =
         | _ -> failwith "word not found")
     search_word
 
+(** check if "MAS" exists in cross shape, from pos. assuming 'a' found at pos.*)
+let cross_mas_check matrix pos =
+  let n_rows = Array.length matrix in
+  let n_cols = Array.length matrix.(0) in
+  if
+    pos.(0) < 1 || pos.(0) >= n_rows - 1 || pos.(1) < 1 || pos.(1) >= n_cols - 1
+  then false
+  else
+    let back =
+      String.concat "A"
+        [
+          String.make 1 matrix.(pos.(0) - 1).(pos.(1) - 1);
+          String.make 1 matrix.(pos.(0) + 1).(pos.(1) + 1);
+        ]
+    in
+    let frwd =
+      String.concat "A"
+        [
+          String.make 1 matrix.(pos.(0) - 1).(pos.(1) + 1);
+          String.make 1 matrix.(pos.(0) + 1).(pos.(1) - 1);
+        ]
+    in
+    if (back = "MAS" || back = "SAM") && (frwd = "MAS" || frwd = "SAM") then
+      true
+    else false
+
 (** iterates through matrix and check if cell is 'X'.
     if found, check if "XMAS" exists in any traversal direction, and add to total. *)
-let process_matrix matrix =
+let process_matrix_1 matrix =
   let words = ref 0 in
   (* this is really messy but all it does is iterate through all elements
      if 'X' found, it traverses every direction to search for word *)
@@ -66,10 +92,25 @@ let process_matrix matrix =
             (Array.iter (fun dir ->
                  try
                    (* this function fails if not found, unit type if found *)
-                   word_exists_in_dir matrix [| i; j |] dir;
+                   xmas_check matrix [| i; j |] dir;
                    words := !words + 1
                  with Failure _ -> ()))
               traversal)
+        row)
+    matrix;
+  !words
+
+(** iterates through matrix and check if cell is 'X'.
+    if found, check if "XMAS" exists in any traversal direction, and add to total. *)
+let process_matrix_2 matrix =
+  let words = ref 0 in
+  (* iterate through all elements, if 'A' found check crossed "MAS" exists, and add to total *)
+  Array.iteri
+    (fun i row ->
+      Array.iteri
+        (fun j cell ->
+          if cell = 'A' && cross_mas_check matrix [| i; j |] then
+            words := !words + 1)
         row)
     matrix;
   !words
@@ -78,4 +119,6 @@ let () =
   let matrix = input_file_to_matrix file_path in
 
   (* part 1 *)
-  Printf.printf "solution to part 1: %d\n" (process_matrix matrix)
+  Printf.printf "solution to part 1: %d\n" (process_matrix_1 matrix);
+  (* part 2 *)
+  Printf.printf "solution to part 1: %d\n" (process_matrix_2 matrix)
